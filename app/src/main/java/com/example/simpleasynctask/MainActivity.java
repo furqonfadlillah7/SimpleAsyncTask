2efdc251ea663d950c64a0bf58cb746dc1d4aada
furqonfadlillah7 <furqonfadlillah7@gmail.com>
2022-10-19 00:00:15 +0700

    first commit

---
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.simpleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mBookInput;
+    private TextView mAuthorText, mTitleText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBookInput = (EditText) findViewById(R.id.bookInput);
+        mAuthorText = (TextView) findViewById(R.id.authorText);
+        mTitleText = (TextView) findViewById(R.id.titleText);
+    }
+
+    public void searchBooks(View view) {
+        String queryString = mBookInput.getText().toString();
+
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected() && queryString.length() != 0){
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if(queryString.length() == 0){
+                mAuthorText.setText("");
+                mTitleText.setText("Please enter a search term");
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText("Please check your network connection and try again");
+            }
+        }
+    }
+}
